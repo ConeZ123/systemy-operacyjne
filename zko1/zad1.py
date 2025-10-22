21ad36d3fd4bf2fb1c9becdeeffc77036bcd0e66
ConeZ123 <nismat2004@gmail.com>
2025-10-22 21:21:37 +0200

    fixing indents

--- zko1/zad1.py
+++ zko1/zad1.py
@@ -5,20 +5,20 @@ import pwd
 def list_processes():
        for pid in os.listdir('/proc'):
               if pid.isdigit():
-                     status = f"/proc/{pid}/status"
-                     comm = f"/proc/{pid}/comm"
+                    status = f"/proc/{pid}/status"
+                    comm = f"/proc/{pid}/comm"
 
-                     with open(status) as file:
-                            for line in file:
-                                if line.startswith('Uid:'):
-                                    uid = int(line.split()[1])
-                                    break
-                            user = pwd.getpwuid(uid).pw_name
+                    with open(status) as file:
+                        for line in file:
+                            if line.startswith('Uid:'):
+                                uid = int(line.split()[1])
+                                break
+                        user = pwd.getpwuid(uid).pw_name
 
-                     with open(comm) as file:
-                            comm = file.read().strip()
+                    with open(comm) as file:
+                        comm = file.read().strip()
 
-                     print(user, pid, comm)
+                    print(user, pid, comm)
 
 if __name__ == "__main__":
     print("USER PID COMMAND")
