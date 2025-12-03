03b0856d202d36afd05f86c562a589449f5df13a
Mateusz Niski <90795379+ConeZ123@users.noreply.github.com>
2025-12-03 19:34:02 +0100

    Refactor sysinfo.sh

--- cwi7/sysinfo.sh
+++ cwi7/sysinfo.sh
@@ -1,88 +1,83 @@
 #!/bin/bash
 
-cpu() {
+get_cpu() {
        echo "CPU: " $(cat /proc/cpuinfo | grep processor | cut -d ':' -f 2)
 }
 
-ram() {
+get_ram() {
        echo "RAM:" $(free -m | grep Mem | awk '{printf "%s/%s MiB (%.0f%% used)\n, $3, $2, ($3/$2*100)}')
 }
 
-load() {
+get_load() {
        echo "Load:" $(uptime | awk -F, '{print $3",",$4",", $5}' | sed 's/load average//g') 
 }
 
-kernel() {
+get_kernel() {
        echo "Kernel:" $(uname -a | awk '{print $1, $3}')
 }
 
-gpu() {
+get_gpu() {
        echo "GPU:" $(lspci | grep -i 'vga' | sed 's/.*: //')
 }
 
-user() {
+get_user() {
        echo "User:" $(echo "$USER")
 }
 
-shell() {
+get_shell() {
        echo "Shell:" $(basename "$SHELL")
 }
 
-processes() {
+get_processes() {
        echo "Processes:" $(ps -aux | wc -l)
 }
 
-threads() {
+get_threads() {
        echo "Threads:" $(ps -eLf | wc -l)
 }
 
-ip() {
+get_ip() {
        echo "IP:" $(ip -o -4 address | awk '{print $4}')
 }
 
-dns() {
+get_dns() {
        echo "DNS:" $(grep nameserver /etc/resolve.conf | awk '{print $2}')
 }
 
-internet() {
+get_internet() {
        echo $(timeout 1 ping -c1 8.8.8.8 >/dev/null && echo "Internet: OK" || echo "Internet: Fail")        
 }
 
-if [ "$#" -eq 0 ]; then
-    cpu
-    ram
-    load
-    kernel
-    gpu
-    user
-    shell
-    processes
-    threads
-    ip
-    dns
-    internet
-    exit 0
+if [ $# -eq 0 ]; then
+    get_cpu
+    get_ram
+    get_load
+    get_kernel
+    get_gpu
+    get_user
+    get_shell
+    get_processes
+    get_threads
+    get_ip
+    get_dns
+    get_internet
+else
+
+    for arg in "$@"; do
+    
+        case "${arg,,}" in
+            cpu) cpu ;;
+            ram) ram ;;
+            load) load ;;
+            kernel) kernel ;;
+            gpu) gpu ;;
+            user) user ;;
+            shell) shell ;;
+            processes) processes ;;
+            threads) threads ;;
+            ip) ip ;;
+            dns) dns ;;
+            internet) internet ;;
+       esac
+   done
 fi
-
-for arg in "$@"; do
-    case ${arg,,} in
-        cpu) cpu ;;
-        ram) ram ;;
-        load) load ;;
-        kernel) kernel ;;
-        gpu) gpu ;;
-        user) user ;;
-        shell) shell ;;
-        processes) processes ;;
-        threads) threads ;;
-        up) ip ;;
-        dns) dns ;;
-        internet) internet ;;
-        *)
-            echo "Invalid argument: $arg"
-            exit 1
-            ;;
-    esac
-done
-
-exit 0
